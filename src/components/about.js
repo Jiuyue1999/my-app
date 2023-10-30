2c7c5c529805c1686b298a24ff2afc4df9ab3c6c
Jiuyue1999 <78697414+Jiuyue1999@users.noreply.github.com>
2023-10-30 19:03:46 -0400

    update about logo

--- src/components/about.js
+++ src/components/about.js
@@ -1,28 +1,33 @@
 import React from 'react';
-
+import logo from '../images/wang.11537.png';
+import logo1 from '../images/zhang.9041.png';
+import logo2 from '../images/hongchen.png';
 function About() {
   // Define an array of team members with their information
   const teamMembers = [
     {
       name: 'ARan',
       jobTitle: 'Chief Technology Officer',
-      education: 'Master\'s in Computer Science, The Ohio State University',
+      education: "Master's in Computer Science, The Ohio State University",
       experience: 'Over 15 years of experience in software development and technology leadership.',
       colorClass: 'bg-primary', // Blue
+      imageSrc: logo,
     },
     {
       name: '65536',
       jobTitle: 'Lead Designer',
       education: 'Bachelor\'s in Game Design, The Ohio State University',
-      experience: '10 years of experience in graphic design with a talent for turning ideas into visually stunning designs.',
+      experience: '10 years of experience in graphic design for turning ideas into visually.',
       colorClass: 'bg-success', // Green
+      imageSrc: logo1,
     },
     {
       name: 'Hong',
       jobTitle: 'Marketing Manager',
       education: 'Bachelor\'s in Computer Engineering, The Ohio State University',
-      experience: 'Marketing expert with a deep understanding of consumer behavior and a track record of successful campaigns.',
+      experience: 'Marketing expert with a deep understanding of consumer behavior.',
       colorClass: 'bg-warning', // Yellow
+      imageSrc: logo2,
     }
   ];
 
@@ -34,6 +39,9 @@ function About() {
         {teamMembers.map((member, index) => (
           <div key={index} className="col-md-4">
             <div className={`card mb-4 ${member.colorClass}`}>
+            <div className="image-container">
+              <img src={member.imageSrc} alt={member.name} className="img-fluid" width="300" height="200" />
+            </div>
               <div className="card-body">
                 <h2 className="card-title text-white">{member.name}</h2>
                 <p className="card-text text-white"><strong>Job Title:</strong> {member.jobTitle}</p>
