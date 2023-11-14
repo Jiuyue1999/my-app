3408320f67d994d78d67f463b35d36e8e63b8ecd
Jiuyue1999 <78697414+Jiuyue1999@users.noreply.github.com>
2023-11-13 21:30:04 -0500

    update cart

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import StepProgressBar from './StepProgressBar';
+
 const steps = [
   { label: 'Purchase', status: 'inactive' },
   { label: 'Cart', status: 'active' },
@@ -9,25 +10,23 @@ const steps = [
   { label: 'Checkout', status: 'inactive' },
   { label: 'Finish', status: 'inactive' },
 ];
+
 function Cart() {
   const [cartItems, setCartItems] = useState([]);
+
   useEffect(() => {
     const storedCartItems = JSON.parse(sessionStorage.getItem('shoppingCart')) || [];
     setCartItems(storedCartItems);
   }, []);
 
   const calculateTotalPrice = () => {
-    return cartItems.reduce((total, item) => total + item.book.price * item.quantity, 0);
+    return cartItems.reduce((total, item) => total + Number(item.book.price) * item.quantity, 0);
   };
 
   return (
     <div className="container">
-
       <h1 className="mt-4">Shopping Cart</h1>
-      <StepProgressBar
-        currentStep={1}
-        steps={steps}
-      />
+      <StepProgressBar currentStep={1} steps={steps} />
       <ul className="list-group">
         {cartItems.map((item, index) => (
           <li key={index} className="list-group-item">
@@ -35,11 +34,11 @@ function Cart() {
               <div className="col-md-8">
                 <h4>{item.book.title}</h4>
                 <p>Author: {item.book.author}</p>
-                <p>Price: ${item.book.price.toFixed(2)}</p>
+                <p>Price: ${Number(item.book.price).toFixed(2)}</p>
                 <p>Quantity: {item.quantity}</p>
               </div>
               <div className="col-md-4 text-right">
-                <h4>${(item.book.price * item.quantity).toFixed(2)}</h4>
+                <h4>${(Number(item.book.price) * item.quantity).toFixed(2)}</h4>
               </div>
             </div>
           </li>
@@ -49,7 +48,9 @@ function Cart() {
       <h2 className="mt-4">Total Price: ${calculateTotalPrice().toFixed(2)}</h2>
 
       {/* Add a "Checkout" button that links to the payment entry page */}
-      <Link to="/paymentEntry" className="btn btn-primary mt-4" >Checkout</Link>
+      <Link to="/paymentEntry" className="btn btn-primary mt-4">
+        Checkout
+      </Link>
     </div>
   );
 }
